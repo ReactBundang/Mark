c077a4a3b835fe3bd6c95c9f8f51529147853be7
Mark Choi <fantasysa@gmail.com>
2020-08-08 01:59:54 +0900

    Use enter to add data as well.

--- new-todo2/src/App.js
+++ new-todo2/src/App.js
@@ -3,17 +3,48 @@ import React, {useState, useEffect} from 'react';
 import './App.css';
 import {fire,getFireDB,setFireDB} from './Firebase'
 import List from './List.jsx'
-import { Button } from '@material-ui/core';
-import { Input } from '@material-ui/core';
 
+import { Button,Input } from '@material-ui/core';
+import Card from '@material-ui/core/Card';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles({
+  root: {
+    minWidth: 275,
+  },
+  bullet: {
+    display: 'inline-block',
+    margin: '0 2px',
+    transform: 'scale(0.8)',
+  },
+  title: {
+    fontSize: 14,
+  },
+  pos: {
+    marginBottom: 12,
+  },
+});
 
 const App= () => {
+  const classes = useStyles();
   const [todos, setTodos] = useState([]);
   const [newTodo, setNewTodo] = useState(); //입력값 저장용.
 
   // input 키입력 onChange 이벤트
   const InputChanged = (e) => {
     setNewTodo(e.target.value);
+  }
+
+  const KeyPressed = (e) => {
+    if(e.key === 'Enter') {
+      e.preventDefault();
+      if(todos!=undefined)
+        setTodos([...todos, newTodo]);
+    }
+    
   }
 
   // firebase library 초기화
@@ -39,14 +70,18 @@ const App= () => {
   }, [todos]);
 
   return (
-    <>
-      <h1>Todo App</h1>
-      <form action="">
+    <Card className={classes.root} id="card_main">
+      <CardContent>
+      <Typography variant="h5" component="h2">Todo App</Typography>
+      <Typography className={classes.title} color="textSecondary" gutterBottom>
+      With react hooks + firebase!</Typography>
+      <form action="" onKeyPress={KeyPressed}>
         <Input type="text" onChange={InputChanged}/>
         <Button onClick={addTodo}>Add</Button>
       </form>
       <List todos={todos}/>
-    </>
+      </CardContent>
+    </Card>
   )
 }
 
