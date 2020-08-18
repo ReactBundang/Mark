5d2fbac82c45a5dc3ed3c405c8e8cbfd3a734948
최준혁 <mark.choi@poscoict.com>
2020-08-18 17:33:08 +0900

    Get values from input.

--- Quiz/src/index.js
+++ Quiz/src/index.js
@@ -4,15 +4,11 @@ import './index.css';
 //import {fire,getFireDB,setFireDB} from './Firebase'
 //import SimpleMap from './SimpleMap'
 
-import { Button,Input } from '@material-ui/core';
-import Card from '@material-ui/core/Card';
-import CardActions from '@material-ui/core/CardActions';
-import CardContent from '@material-ui/core/CardContent';
-import Typography from '@material-ui/core/Typography';
+import { Button,Input, Card, CardActions, CardContent,Typography } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
 
 import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
-import Home from './Home';
+import LoginPage from './LoginPage';
 import GameMain from './GameMain';
 
 const App= () => {
@@ -33,14 +29,14 @@ const App= () => {
     <div>
       <ul>
         <li>
-          <Link to="/single">Singleplay</Link>
+          <Link to="/login">Login</Link>
         </li>
         <li>
           <Link to="/multi">Multiplay</Link>
         </li>
       </ul>
       <hr />
-      <Route path="/single" exact={true} component={Home} />
+      <Route path="/login" exact={true} component={LoginPage} />
       <Route path="/multi" component={GameMain} />
     </div>
     </Router>
